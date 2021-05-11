961c62e00d894777f33a89a4284a7adfac5ffef8
Jan Lishak <35602700+janlishak@users.noreply.github.com>
2021-05-11 20:28:50 +0200

    ok, it doesn't work as expected ..

--- src/UtopiaCoins.java
+++ src/UtopiaCoins.java
@@ -2,64 +2,21 @@ import java.util.*;
 
 public class UtopiaCoins
 {
-  HashMap<Integer, int[]> map = new HashMap<>();
+  private HashMap<Integer, int[]> map = new HashMap<>();
+  private int functionCalls = 0;
 
-  public static void main(String[] args)
-  {
-    UtopiaCoins utopiaCoins = new UtopiaCoins();
-    int[] coinValues = new int[] {22, 10, 7, 1};
-    int[] result = utopiaCoins.getCounts(coinValues, 42);
-    printArray(result);
-    printCalculated(utopiaCoins.map);
-    System.out.println("second");
-    result = utopiaCoins.getCounts(coinValues, 643922374);
-    printArray(result);
-    printCalculated(utopiaCoins.map);
-  }
-
-  private int[] save(int key, int[] result, int pointer){
-    int[] trim = new int[result.length];
-    int sum = 0;
-    for (int i = pointer; i < result.length-1; i++)
-    {
-      trim[i] = result[i];
-      sum+= result[i];
-    }
-    trim[trim.length-1]=sum;
-    map.put(key, trim);
-    return result;
-  }
-
-  private int[] get(int key, int[] result, int pointer){
-    int[] saved = map.get(key);
-    System.out.println("send : " + Arrays.toString(result));
-    System.out.println("found: " + Arrays.toString(saved));
-    for (int i = pointer; i < result.length; i++)
-    {
-      result[i]+=saved[i];
-    }
-    System.out.println("resul: " + Arrays.toString(result));
-    return result;
-  }
-
-  private static void printCalculated(HashMap<Integer, int[]> map){
-    for( Map.Entry<Integer, int[]> entry : map.entrySet() ){
-      System.out.println( entry.getKey() + " => " + Arrays.toString(entry.getValue()));
-    }
-  }
-
-  private static void printArray(int[] array){
-    System.out.println(Arrays.toString(array));
-  }
-
-
+  //this function will start the recursion
   public int[] getCounts(int[] coinValues, int number)
   {
     int[] result = getCountsRecursive(coinValues, number, new int[coinValues.length + 1], 0);
+    if(number == 0) System.out.println("fuck");
     return save(number,result, 0);
   }
 
+
+  //the main recursion method
   private int[] getCountsRecursive(int[] coinValues, int number, int[] result, int pointer){
+    functionCalls++;
     //check if we haven't already calculated this value:
     if(map.get(number)!=null )
       return get(number,result,pointer);
@@ -78,13 +35,40 @@ public class UtopiaCoins
     //case: normal case, where each bigger coin has at least double the value of the previous one
     result[pointer] += number/coinValues[pointer];
     result[coinValues.length]+=result[pointer];
-    if (pointer+1 == coinValues.length)
+    if (pointer+1 == coinValues.length | number == 0)
       return result;
     return save(number, getCountsRecursive(coinValues, number%coinValues[pointer], result, pointer+1), pointer);
   }
 
-
-
-
-
+  //saves the calculated value for feature use
+  private int[] save(int key, int[] result, int pointer){
+    int[] trim = new int[result.length];
+    int sum = 0;
+    for (int i = pointer; i < result.length-1; i++)
+    {
+      trim[i] = result[i];
+      sum+= result[i];
+    }
+    trim[trim.length-1]=sum;
+    map.put(key, trim);
+    return result;
   }
+
+  //helps to reuse already calculated values
+  private int[] get(int key, int[] result, int pointer){
+    int[] saved = map.get(key);
+    for (int i = pointer; i < result.length; i++) result[i]+=saved[i];
+    return result;
+  }
+
+  //helper functions
+  public void printCalculated(){
+    for( Map.Entry<Integer, int[]> entry : map.entrySet() ){
+      System.out.println( entry.getKey() + " => " + Arrays.toString(entry.getValue()));
+    }
+  }
+
+  public static void printArray(int[] array){
+    System.out.println(Arrays.toString(array));
+  }
+}
---
+++ test/UtopiaCoinTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UtopiaCoinTest
+{
+     //this one is hard to test since there is a possibility that there can be multiple good results with the same optimal number of coins
+     //example value: 30
+     //coins      : 22, 10,  7,  1
+     //this       :  1,  0,  1,  1
+     //but also   :  0,  3,  0,  0
+     //both use 3 coins but are different solutions
+
+     private UtopiaCoins utopiaCoins;
+
+
+     @BeforeEach
+     public void setup()
+     {
+          utopiaCoins = new UtopiaCoins();
+     }
+
+     @AfterEach
+     public void destroy(){
+          utopiaCoins.printCalculated();
+          utopiaCoins = null;
+     }
+
+     @Test
+     public void testValues1(){
+          int number = 30;
+          int[] coinValues = new int[] {22, 10, 7, 1};
+          int[] expected = new int[] {1, 0, 1, 1};
+          int[] result = utopiaCoins.getCounts(coinValues, number);
+
+          assertTrue(compareTwoArrays(expected,result));
+     }
+
+     @Test
+     public void testValues2(){
+          int number = 15;
+          int[] coinValues = new int[] {22, 10, 7, 1};
+          int[] expected = new int[] {0, 0, 2, 1};
+          int[] result = utopiaCoins.getCounts(coinValues, number);
+
+          assertTrue(compareTwoArrays(expected,result));
+     }
+
+     @Test
+     public void testValues3(){
+          int number = 32;
+          int[] coinValues = new int[] {22, 10, 7, 1};
+          int[] expected = new int[] {1, 1, 0, 0};
+          int[] result = utopiaCoins.getCounts(coinValues, number);
+
+          assertTrue(compareTwoArrays(expected,result));
+     }
+
+     private boolean compareTwoArrays(int[] arrA, int[] arrB){
+          //second can be bigger, will evaluate the first values
+          for (int i = 0; i < arrA.length; i++) if (arrA[i] != arrB[i]) return false;
+          return true;
+     }
+
+}
